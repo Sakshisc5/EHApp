341d86dfc85afa58f8a66968e5d73cd1bae97f11
Sakshisc5 <139097319+Sakshisc5@users.noreply.github.com>
2024-10-26 17:55:58 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from workout import home_page
+from sign_in import signin_page
+
+def main():
+    # Initialize the session state if it doesn't exist
+    if 'page' not in st.session_state:
+        st.session_state.page = "workout"  # Default to home page
+
+    # Render the appropriate page based on session state
+    if st.session_state.page == "workout":
+        home_page()
+    elif st.session_state.page == "sign_in":
+        signin_page()
+
+if __name__ == "__main__":
+    main()
