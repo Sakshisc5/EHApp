b474a448cd7b639b5723f24eb46df65edef38d99
Sakshisc5 <139097319+Sakshisc5@users.noreply.github.com>
2024-10-26 23:50:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from workout import home_page
 from sign_in import signin_page
+from jjMotion import jjMotion_page
+from pMotion import pMotion_page
+from bcMotion import bcMotion_page
 
 def main():
-    st.markdown('<link rel="stylesheet" href="stylesheet.css">', unsafe_allow_html=True)
-
     # Initialize the session state if it doesn't exist
     if 'page' not in st.session_state:
         st.session_state.page = "workout"  # Default to home page
@@ -14,6 +15,12 @@ def main():
         home_page()
     elif st.session_state.page == "sign_in":
         signin_page()
+    elif st.session_state.page == "Jumping Jacks Counter":
+        jjMotion_page()
+    elif st.session_state.page == "Plank Checker":
+        pMotion_page()
+    elif st.session_state.page == "Bicep Curl Form":
+        bcMotion_page()
 
 if __name__ == "__main__":
     main()
