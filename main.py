257faff3567fe7934a395271f27b42238f20736a
Sakshisc5 <139097319+Sakshisc5@users.noreply.github.com>
2024-10-26 23:28:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from workout import home_page
 from sign_in import signin_page
+from jjMotion import jjMotion_page
+from pMotion import pMotion_page
+from bcMotion import bcMotion_page
 
 def main():
     # Initialize the session state if it doesn't exist
@@ -12,6 +15,13 @@ def main():
         home_page()
     elif st.session_state.page == "sign_in":
         signin_page()
+    elif st.session_state.page == "Jumping Jacks Counter":
+        jjMotion_page()
+    elif st.session_state.page == "Plank Checker":
+        pMotion_page()
+    elif st.session_state.page == "Bicep Curl Form":
+        bcMotion_page()
 
 if __name__ == "__main__":
     main()
+
